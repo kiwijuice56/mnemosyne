6350eeeb37787d463482ecdf9cb9fa6a9279381a
kiwijuice56 <alfaroeric127@gmail.com>
2024-06-19 17:32:40 -0500

    Player hurt sound

--- main/spike/spike.gd
+++ main/spike/spike.gd
@@ -4,11 +4,15 @@ extends CharacterBody2D
 @export var init_velocity: Vector2 
 @export var reflect_h: bool = false
 @export var reflect_v: bool = false
+
+var stuck: bool = false
  
 func slice() -> void:
 	%AnimationPlayer.speed_scale = 0.1
-	await get_tree().create_timer(0.2).timeout
-	%AnimationPlayer.speed_scale = 1.0
+	stuck = true
+	await get_tree().create_timer(0.1).timeout
+	%AnimationPlayer.speed_scale = 1.5
+	stuck = false
 
 func _physics_process(delta: float) -> void:
 	if init_velocity.x > 0 and %Right.is_colliding() or init_velocity.x < 0 and %Left.is_colliding():
@@ -17,7 +21,7 @@ func _physics_process(delta: float) -> void:
 	if init_velocity.y > 0 and %Bottom.is_colliding() or init_velocity.y < 0 and %Top.is_colliding():
 		init_velocity.y *= -1
 		clang()
-	velocity = init_velocity
+	velocity = init_velocity if not stuck else Vector2()
 	move_and_slide()
 
 func clang() -> void:
